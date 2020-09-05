75f6e79f8340d17da3514f2041b8e40d52337834
liurongqing <phpgege@gmail.com>
2020-09-06 01:52:33 +0800

    Initial commit

---
+++ build/webpack.prod.js
@@ -0,0 +1,21 @@
+const { merge } = require('webpack-merge')
+const CopyPlugin = require('copy-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const common = require('./webpack.common.js')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+
+module.exports = merge(common, {
+  mode: 'production',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [{
+        from: 'public',
+        to: './'
+      }]
+    }),
+    // new BundleAnalyzerPlugin()
+  ],
+
+})
